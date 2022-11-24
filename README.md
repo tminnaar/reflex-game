0540021f84897ec8e18391b923489b9b2e93a3ba
tminnaar <tminnaar94@gmail.com>
2022-11-24 22:15:06 +0000

    README update and js comments

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Reflex Game
 
-A reaction time based game with each rounding lasting 30 seconds. The goal is to click as many targets within the time alloted while the targets randomly resize and reposition when clicked. On the left is a panel that display the time remaining, the current score and the high score.
+A reaction time-based game with each rounding lasting 30 seconds. The goal is to click as many targets within the allotted time while the targets randomly resize and
+reposition. The panel on the right displays the time remaining, current score and high score which are updated using DOM Manipulation. setInterval is used to
+countdown the time remaining and update the value in the display panel. setTimeout stops the game, removes the target and allows the user to start again.
--- reflex-game.js
+++ reflex-game.js
@@ -16,6 +16,7 @@ let currentScore = 0;
 let timeRemaining = 0;
 let currentHighScore = 0;
 
+//clicking the target randomises its size and position within a range. string interpolation pushes the generated values into the stylings. score value is increased when clicked.
 function targetClick() {
 	const targetSize = Math.floor(Math.random() * (maxTargetSize - minTargetSize)) + minTargetSize;
 	target.style.width = target.style.height = `${targetSize}px`;
@@ -30,26 +31,34 @@ function targetClick() {
 	scoreDisplay.textContent = currentScore;
 }
 
+//initialises the game, resets the timer and current score.
 function startGame() {
 	currentScore = 0;
 	scoreDisplay.textContent = currentScore;
+
+	//hides the menu pop-up.
 	target.style.display = "block";
 	gamePopUp.style.display = "none";
 
 	timeRemaining = timerMaxLength;
+
+	//interpolates the remaining time.
 	timeDisplay.textContent = `${timeRemaining}s`;
 
+	//updates the time remaning value every second.
 	const timerInterval = setInterval(() => {
 		timeRemaining--;
 		timeDisplay.textContent = `${timeRemaining}s`;
 	}, 1000);
 
+	//on time-out hides the target, displays the pop-up and alters the text to "Try Again?"
 	setTimeout(() => {
 		clearInterval(timerInterval);
 		target.style.display = "none";
 		gamePopUp.style.display = "flex";
 		gamePopUpText.textContent = "Try Again?";
 
+		//if the high score is surpassed, the high score display on the left is updated and on time-out the pop-up text becomes "🎉New High Score!🎉Try Again?".
 		if (currentScore > currentHighScore) {
 			currentHighScore = currentScore;
 			highScoreDisplay.textContent = currentHighScore;
